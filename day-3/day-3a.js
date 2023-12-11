205386436b1596c07a0c66d34f213140c038967c
felixreverett <felixreverett@gmail.com>
2023-12-11 13:58:28 +0000

    day3a progress

--- day-3/day-3a.js
+++ day-3/day-3a.js
@@ -22,25 +22,108 @@ function DayThree()
 function DayThree2()
 {
     let lines = fs.readFileSync("day-3/input-2023-3.txt", "utf-8").split("\n");
+    
+    // create validator which will be used to pre-determine if a value is a number
     let validator = new Array(lines.length);
     for (let i = 0; i < lines.length; i++)
     {
         validator[i] = new Array(lines[i].length).fill(0);
     }
 
-    console.log(validator[1][1]);
+    //console.log(validator[0][13]);
 
-    let numberRegex = /\d/;
+    let numberRegex = /[0-9]/;
     for (let row = 0; row < lines.length; row++)
     {
-        for (let col = 0; col < row.length; col++)
+        for (let col = 0; col < lines[row].length; col++)
         {
-            if (numberRegex.test(lines))
+            if (numberRegex.test(lines[row][col]))
             {
-
+                validator[row][col] = 1;
+                //console.log(`Success! This value is a number: ${lines[row][col]}`);
+            }
+            else
+            {
+                //console.log(lines[row][col]);
             }
         }
     }
+
+    //console.log(validator[0][13]);
+
+    // Iterate through every validated value and  
 }
 
-DayThree2();
+function DayThree3()
+{
+    //identify all symbols
+    let lines = fs.readFileSync("day-3/input-2023-3.txt", "utf-8").split("\n");
+    
+    // create validator which will be used to pre-determine if a value is a number
+    let validator = new Array(lines.length);
+    for (let i = 0; i < lines.length; i++)
+    {
+        validator[i] = new Array(lines[i].length).fill("."); // mark as . as filler
+    }
+
+    let numberRegex = /[^0-9.]/;
+    for (let row = 0; row < lines.length; row++)
+    {
+        for (let col = 0; col < lines[row].length; col++)
+        {
+            if (numberRegex.test(lines[row][col]))
+            {
+                console.log("A number has been found");
+                validator[row][col] = "S"; // mark as S for Symbol
+                // top row
+                if (row > 0)
+                {
+                    if (col > 0)
+                    {
+                        validator[row-1][col-1] = "V"; // mark as V for Validated
+                    }
+                    validator[row-1][col] = "V";
+                    if (col < lines[row-1].length-1)
+                    {
+                        validator[row-1][col+1] = "V";
+                    }
+                }
+                // middle row
+                if (col > 0)
+                {
+                    validator[row][col-1] = "V";
+                }
+                if (col < lines[row].length-1)
+                {
+                    validator[row][col+1] = "V";
+                }
+                // bottom row
+                if (row < lines.length - 1)
+                {
+                    if (col > 0)
+                    {
+                        validator[row + 1][col - 1] = "V";
+                    }
+                    validator[row + 1][col] = "V";
+                    if (col < lines[row - 1].length - 1)
+                    {
+                        validator[row + 1][col + 1] = "V";
+                    }
+                }
+            }
+        }
+    }
+
+    // test it
+    for (let row = 0; row < validator.length; row++)
+    {
+        tempRow = "";
+        for (let i = 0; i < validator[row].length; i++)
+        {
+            tempRow += validator[row][i];
+        }
+        console.log(tempRow);
+    }
+}
+
+DayThree3();
