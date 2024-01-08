2f531b4e24ba72a267fbcc755fb5d95c42469512
felixreverett <felixreverett@gmail.com>
2024-01-08 20:17:13 +0000

    day4part1 further progress

--- day-4/day-4a.js
+++ day-4/day-4a.js
@@ -17,6 +17,36 @@ function DayFour()
 
     // Array is now 2D but individual numbers are not yet separated -> I'll do this next
 
+    var newLines = [];
+    for (let line = 0; line < lines.length; line++) {
+        newLines[line] = [];
+        newLines[line][0] = lines[line][0].split(" ");
+        newLines[line][1] = lines[line][1] ? lines[line][1].split(" ") : [];
+    }
+
+    // Calculating score
+    let totalPoints = 0;
+
+    for (let line = 0; line < newLines.length; line++)
+    {
+        let tally = 0;
+        //console.log(`Line ${line}: ${}`)
+        for (let j = 0; j < newLines[line][0].length; j++)
+            {
+                if (newLines[line][1].includes(newLines[line][0][j]))
+                {
+                    tally++;
+                }
+            }
+
+        if (tally > 0)
+        {
+            totalPoints += 2 ** (tally - 1)
+        }
+        console.log(`The total for line ${line} is ${tally} for a total of ${totalPoints}.`)
+    }
+
+    console.log(`Final total: ${totalPoints}`);
 }
 
 DayFour();
