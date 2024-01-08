de51dd4f9fe484676d6eb2319ccf0d15174892f3
Felix Everett <felixreverett@gmail.com>
2024-01-08 21:00:32 +0000

    day4part1 solution

--- day-4/day-4a.js
+++ day-4/day-4a.js
@@ -1,23 +1,16 @@
-//day 4
+//day 4a
 var fs = require("fs"); // imports fs
-
-// parse input for each day into winning numbers + your numbers
-// for each day, check each number of winning numbers against your numbers, and keep a tally
-// if tally > 0, add to answer 1 * 2 ^ (tally - 1)
-// return answer
 
 function DayFour()
 {
     // 1) first identify all symbols and mark around them in an array the same size as the input
-
     let lines = fs.readFileSync("day-4/input-2023-4.txt", "utf-8")
         .replace(/Card \d*: /g, "")
         .split("\r\n")
         .filter(line => line.trim() !== "")
         .map(line => line.split(" | "));
 
-    // Array is now 2D but individual numbers are not yet separated -> I'll do this next
-
+    // 2) Array is now 2D but individual numbers are not yet separated -> I'll do this next
     var newLines = [];
     for (let line = 0; line < lines.length; line++) {
         newLines[line] = [];
@@ -25,18 +18,17 @@ function DayFour()
         newLines[line][1] = lines[line][1] ? lines[line][1].split(" ") : [];
     }
 
-    // Calculating score
+    // 3) Calculate score
     let totalPoints = 0;
 
     for (let line = 0; line < newLines.length; line++)
     {
         let tally = 0;
-        //console.log(`Line ${line}: ${}`)
+        
         for (let j = 0; j < newLines[line][0].length; j++)
             {
                 if (newLines[line][1].includes(newLines[line][0][j]))
                 {
-                    console.log(`> ${newLines[line][0][j]} has a match!`);
                     tally++;
                 }
             }
@@ -45,8 +37,8 @@ function DayFour()
         {
             totalPoints += 2 ** (tally - 1)
         }
+        
         console.log(`Line: ${line} | Matches: ${tally} | Running total of ${totalPoints}.`)
-        //console.log(newLines[line]);
     }
 
     console.log(`Final total: ${totalPoints}`);
