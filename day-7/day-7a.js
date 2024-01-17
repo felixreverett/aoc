1e04529dc62c9c627e953edd858c5c9147b68d47
Felix Everett <felixreverett@gmail.com>
2024-01-17 21:48:12 +0000

    day7a prep

---
+++ day-7/day-7a.js
@@ -0,0 +1,16 @@
+//day 7a
+
+/* Solution Plan:
+>
+*/
+var fs = require("fs"); // imports fs
+
+function DaySix()
+{
+    let lines = fs.readFileSync("day-6/input-2023-6.txt", "utf-8")
+        .replace("\r", "")
+        .split("\n");
+   
+}
+
+DaySeven();
