0f6850610b503c0288221162e807d4257dd67151
Felix Everett <felixreverett@gmail.com>
2024-02-07 08:38:01 +0000

    Day X1a progress

--- day-X1/day-X1a.js
+++ day-X1/day-X1a.js
@@ -15,11 +15,22 @@ function DayEleven()
     .map(i => i.split(""));
 
   let expandedSpace = expandSpace(space);
+
+  // debug to see if it worked
+  for (let i = 0; i < expandedSpace.length; i++)
+  {
+    let row = "";
+    for (let j = 0; j < expandedSpace[i].length; j++)
+    {
+      row += expandedSpace[i][j];
+    }
+    console.log(row);
+  }
 }
 
 function expandSpace(space)
 {
-  // get longest row length
+  // prepare columnTracker, get longest row length
   let columnTracker = [];
   for (let c = 0; c < Math.max(...(space.map(el => el.length))); c++)
   {
@@ -39,7 +50,7 @@ function expandSpace(space)
   }
 
   // expand columns at appropriate indices
-  for (let col = columnTracker.length - 1; col > 0; col--)
+  for (let col = columnTracker.length - 1; col >= 0; col--)
   {
     if (columnTracker[col] === 0)
     {
@@ -50,7 +61,40 @@ function expandSpace(space)
     }
   }
 
-  console.log(space);
+  console.log(space); //debug
+
+  // prepare rowTracker
+  let rowTracker = [];
+  for (let r = 0; r < space.length; r++)
+  {
+    rowTracker.push(0);
+  }
+
+  // mark all rows that have at least 1 galaxy
+  for (let row = 0; row < space.length; row++)
+  {
+    for (let col = 0; col < space[row].length; col++)
+    {
+      if (space[row][col] === "#")
+      {
+        rowTracker[row] = 1;
+      }
+    }
+  }
+
+  // expand rows at appropriate indices
+  for (let row = rowTracker.length - 1; row >= 0; row--)
+  {
+    if (rowTracker[row] === 0)
+    {
+      let newRow = space[row];
+      space.splice(row, 0, newRow);
+    }
+  }
+
+  console.log(space); //debug
+
+  return space;
 }
 
 DayEleven();
