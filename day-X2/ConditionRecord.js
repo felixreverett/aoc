57e13cacc183bed13acee9a420519a7fcd007682
felixreverett <felixreverett@gmail.com>
2024-02-11 11:50:42 +0000

    Day X2 sample-data solution

--- day-X2/ConditionRecord.js
+++ day-X2/ConditionRecord.js
@@ -1,10 +1,52 @@
 class ConditionRecord
 {
-    constructor(condition, contiguousGroups)
+    constructor(record, contiguousGroups)
     {
-        this.condition = condition; // string
+        this.record = record; // string
         this.contiguousGroups = contiguousGroups; // list
-        this.possibleArrangements;
+        this.possibleArrangements = 0;
+        this.CalculateArrangements(this.record);
+    }
+
+    CalculateArrangements(record)
+    {
+        let indexOfQ = record.indexOf("?");
+
+        if (indexOfQ !== -1)
+        {
+            let newRecordA = record.replace("?", ".");
+            this.CalculateArrangements(newRecordA);
+
+            let newRecordB = record.replace("?", "#");
+            this.CalculateArrangements(newRecordB);
+        }
+
+        else
+        {
+            //console.log(`Arrangement found at ${record}`);
+            // test if valid. increment if valid.
+            let brokenRecord = record.split(".").filter(i => i !== "");
+            //console.log(`This is split into ${brokenRecord}`);
+            let valid = false;
+            if (this.contiguousGroups.length === brokenRecord.length)
+            {
+                valid = true;
+
+                for (let i = 0; i < brokenRecord.length; i++)
+                {
+                    if (brokenRecord[i].length !== this.contiguousGroups[i])
+                    {
+                        valid = false;
+                    }
+                }
+            }
+
+            if (valid)
+            {
+                console.log(`Valid arrangement found at ${brokenRecord}`);
+                this.possibleArrangements++;
+            }
+        }
     }
 }
 
--- day-X2/day-X2a.js
+++ day-X2/day-X2a.js
@@ -1,6 +1,9 @@
 // day X2a
 // 1. Break up input into ConditionRecords
 // 2. For each conditionRecord, calculate possible permutations. Do this inside each class instance for (what I consider to be) cleaner code
+//    a. For every ?, try a permutation where it is . and try a permutation where it is #
+//    b. if the arrangement of #'s is valid in this permutation, increment an existing tally of permutations. Else, don't
+// Note: There may be around 250 million calculations required with this solution.
 
 var fs = require("fs"); // imports fs
 const ConditionRecord = require("./ConditionRecord.js");
@@ -19,10 +22,17 @@ function DayTwelve()
     let currentRecord = records[r].split(" ");
     let condition = currentRecord[0];
     let contiguousGroups = currentRecord[1].split(",").map(i => parseInt(i));
+    console.log(`Calculating: ${r + 1}/${records.length}`);
     conditionRecordList.push(new ConditionRecord(condition, contiguousGroups));
   }
 
-  console.log(conditionRecordList[0]);
+  let sumOfArrangements = 0;
+  for (let r = 0; r < conditionRecordList.length; r++)
+  {
+    sumOfArrangements += conditionRecordList[r].possibleArrangements;
+  }
+  console.log(`The total sum of possible arrangements and solution is: ${sumOfArrangements}`);
+
 }
 
 DayTwelve();
