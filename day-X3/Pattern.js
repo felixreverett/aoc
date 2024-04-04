5b0b0e7a12d1faebbe10cdfda4977d1035b08067
Felix Everett <felixreverett@gmail.com>
2024-04-04 13:19:47 +0100

    Day X3a solution

--- day-X3/Pattern.js
+++ day-X3/Pattern.js
@@ -10,11 +10,27 @@ class Pattern
 
        this.symmetryType;
        this.a;
+       this.b;
        this.solution;
-       this.AttemptVerticalSymmetry();
+
+       if (this.AttemptVerticalSymmetry())
+       {
+           this.solution = (this.a + 1);
+           console.log(this.a);
+           console.log(this.solution);
+       }
+
+       else if (this.AttemptHorizontalSymmetry())
+       {
+            this.solution = (this.a + 1) * 100;
+            console.log(this.a);
+            console.log(this.solution);
+       }
        
-       console.log(this.a);
-       console.log(this.solution);
+       else
+       {
+            throw new console.error("No symmetry found. You shouldn't be here");
+       }
     }
 
     // attempt vertical symmetry
@@ -37,6 +53,7 @@ class Pattern
             {
                 this.symmetryType = "vertical";
                 this.a = a;
+                this.b = b;
                 return true;
             }
         }
@@ -88,6 +105,72 @@ class Pattern
 
         console.log(`> Full Row is mirrored. Returning true`); //debug
         return true; // full row is symmetrical along symmetry ab
+    }
+
+    AttemptHorizontalSymmetry()
+    {
+        // 1. Iterate through every ab pair along the leftmost col
+        for (let a = 0; a < this.numberOfRows - 1; a++)
+        {
+            let b = a + 1; let boundsLimit = Math.min(a, this.numberOfRows - 1 - b);
+
+            // 2. Test every following col for symmetry mirrored at ab
+            if(this.ColumnsMatch(a, b, boundsLimit))
+            {
+                this.symmetryType = "horizontal";
+                this.a = a;
+                this.b = b;
+                return true;
+            }
+        }
+
+        // 3. Return false if no horizontal symmetry is found. This shouldn't happen
+        return false;
+    }
+
+    ColumnsMatch(a, b, boundsLimit)
+    {
+        // 1. Iterate through every col from 0 and test if the col is mirrored at ab
+        console.log(`Trying a ColumnsMatch at a: ${a}, b: ${b}.`); //debug
+        for (let col = 0; col < this.numberOfCols; col++)
+        {
+            if (this.IsColumnMirrored(boundsLimit, col, a, b))
+            {
+                console.log(`> Column ${col} is mirrored`); //debug
+                // proceed to next col
+            }
+
+            else
+            {
+                // 2. If any col is not mirrored, there is no horizontal symmetry at ab -> return false
+                console.log(`> No line of symmetry found on column ${col} at ab = ${a}, ${b}`);
+                return false;
+            }
+        }
+
+        // 3. If all cols are mirrored, there is horizontal symmetry at ab -> return true
+        console.log(`All cols at ab ${a}, ${b} are mirrored.`);
+        return true;
+    }
+
+    IsColumnMirrored(boundsLimit, col, a, b)
+    {
+        for (let checkAdjacent = 0; checkAdjacent <= boundsLimit; checkAdjacent++)
+        {
+            if (this.patternData[a - checkAdjacent][col] != this.patternData[b + checkAdjacent][col])
+            {
+                console.log(`>> Opposing values on col ${col} are not symmetrical. Returning false`); //debug
+                return false;
+            }
+
+            else
+            {
+                console.log(`>> These are the same char: ${this.patternData[a - checkAdjacent][col]} ${this.patternData[b + checkAdjacent][col]}`);
+            }
+        }
+
+        console.log(`> Full Column is mirrored. Returning true`); //debug
+        return true; // full col is symmetrical along symmetry ab
     }
 }
 
--- day-X3/day-X3a.js
+++ day-X3/day-X3a.js
@@ -11,7 +11,7 @@ const Pattern = require("./Pattern.js");
 function DayThirteen()
 {
   // 1. Separate patterns
-  let patterns = fs.readFileSync("day-X3/sample-input-2023-13.txt", "utf-8")
+  let patterns = fs.readFileSync("day-X3/input-2023-13.txt", "utf-8")
   .replace(/\r/gm, "")
   .split("\n\n");
 
@@ -28,6 +28,14 @@ function DayThirteen()
     console.log(newPatternData);
     patternList.push(new Pattern(pat, newPatternData));
   }
+
+  let solution = 0;
+  for (let pat = 0; pat < patternList.length; pat++)
+  {
+    solution += patternList[pat].solution;
+  }
+
+  console.log(`Day 13a solution: ${solution}`);
 
 }
 
