c00ac46d87ca73dd6aeac888a89a1205d986c448
felixreverett <felixreverett@gmail.com>
2024-04-08 13:58:56 +0100

    Day X4a solution

--- day-X4/day-X4a.js
+++ day-X4/day-X4a.js
@@ -9,8 +9,6 @@ function DayFourteen()
   .replace(/\r/gm, "")
   .split("\n");
 
-  console.log(startReflectorDish);
-
   // 2. Make endReflectorDish & lowestRockRow
   let endReflectorDish = [];
   let lowestRockRow = [];
@@ -26,47 +24,23 @@ function DayFourteen()
     endReflectorDish.push(row);
   }
 
-  console.log(endReflectorDish);
-  console.log(lowestRockRow);
-
   // 3. Fill in endReflectorDish
-
   for (let r = 0; r < startReflectorDish.length; r++)
   {
-    console.log(`Row ${r}`);
     for (let c = 0; c < startReflectorDish[r].length; c++)
     {
       if (startReflectorDish[r][c] === "O")
       {
-        console.log(`This value should be an O: ${startReflectorDish[r][c]}`);
-        console.log(`Current column: ${c}, lowestRockRow[${c}]: ${lowestRockRow[c]}`);
-        console.log(lowestRockRow);
-        console.log(endReflectorDish[lowestRockRow[c]][c]);
         endReflectorDish[lowestRockRow[c]][c] = "O";
-        console.log(endReflectorDish[lowestRockRow[c]][c]);
         lowestRockRow[c] += 1;
       }
 
       else if (startReflectorDish[r][c] === "#")
       {
-        console.log(`This value should be #: ${startReflectorDish[r][c]}`);
-
         endReflectorDish[r][c] = "#";
         lowestRockRow[c] = r + 1;
       }
     }
-  }
-
-  console.log(endReflectorDish);
-
-  for (let r = 0; r < endReflectorDish.length; r++)
-  {
-    let row = "";
-    for (let c = 0; c < endReflectorDish[r].length; c++)
-    {
-      row += endReflectorDish[r][c];
-    }
-    console.log(row);
   }
 
   // 4. Find solution
