688b634e9f08b2ddff8ac68a48474be5be4969fa
felixreverett <felixreverett@gmail.com>
2024-04-15 13:56:25 +0100

    Day X5a solution

--- day-X5/day-X5a.js
+++ day-X5/day-X5a.js
@@ -1,62 +1,33 @@
-// day X4a
+// day X5a
 
 var fs = require("fs"); // imports fs
 
-function DayFourteen()
+function DayFifteen()
 {
   // 1. Parse input into startReflectorDish
-  let startReflectorDish = fs.readFileSync("day-X4/input-2023-14.txt", "utf-8")
-  .replace(/\r/gm, "")
-  .split("\n");
+  let initialisationSteps = fs.readFileSync("day-X5/input-2023-15.txt", "utf-8")
+  .replace(/\r/gm, "").replace(/\n/gm, "")
+  .split(",");
 
-  // 2. Make endReflectorDish & lowestRockRow
-  let endReflectorDish = [];
-  let lowestRockRow = [];
-  for (let r = 0; r < startReflectorDish.length; r++)
+  let valueOfSteps = [];
+
+  for (let step = 0; step < initialisationSteps.length; step++)
   {
-    lowestRockRow.push(0);
-    let row = [];
-    for (let c = 0; c < startReflectorDish[r].length; c++)
+    let currentValue = 0;
+    for (let char = 0; char < initialisationSteps[step].length; char++)
     {
-      row.push(".");
+      let asciiValue = initialisationSteps[step].charCodeAt(char);
+      currentValue += asciiValue;
+      currentValue *= 17;
+      currentValue = currentValue % 256;
     }
-
-    endReflectorDish.push(row);
+    valueOfSteps.push(currentValue);
   }
 
-  // 3. Fill in endReflectorDish
-  for (let r = 0; r < startReflectorDish.length; r++)
-  {
-    for (let c = 0; c < startReflectorDish[r].length; c++)
-    {
-      if (startReflectorDish[r][c] === "O")
-      {
-        endReflectorDish[lowestRockRow[c]][c] = "O";
-        lowestRockRow[c] += 1;
-      }
+  let totalSum = valueOfSteps.reduce((accumulator, current) => accumulator + current, 0);
 
-      else if (startReflectorDish[r][c] === "#")
-      {
-        endReflectorDish[r][c] = "#";
-        lowestRockRow[c] = r + 1;
-      }
-    }
-  }
-
-  // 4. Find solution
-  let solution = 0;
-  for (let r = 0; r < endReflectorDish.length; r++)
-  {
-    for (let c = 0; c < endReflectorDish[r].length; c++)
-    {
-      if (endReflectorDish[r][c] === "O")
-      {
-        solution += (endReflectorDish.length - r);
-      }
-    }
-  }
-  console.log(`Day 14a solution: ${solution}`);
+  console.log(`The total sum is: ${totalSum}`);
 
 }
 
-DayFourteen();
+DayFifteen();
