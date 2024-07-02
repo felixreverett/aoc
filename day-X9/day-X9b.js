3cf2eec7194963bb5a77e948ff52c55cbefbeff3
Felix Everett <felixreverett@gmail.com>
2024-07-02 21:05:25 +0100

    Day X9b progress

--- day-X9/day-X9b.js
+++ day-X9/day-X9b.js
@@ -23,13 +23,158 @@ function DayNineteenB()
   .map(i => i.split("\n"));
 
   // 2. Create list of Workflows
-  let Workflows = [];
+  let workflows = [];
   for (let w = 0; w < WorkflowsInput.length; w++)
   {
     let [name, conditions] = WorkflowsInput[w].replace("}", "").split("{");
-    Workflows.push(new Workflow(name, conditions));
+    workflows.push(new Workflow(name, conditions));
   }
 
+  ProcessCondition("in", 1, 4000, 1, 4000, 1, 4000, 1, 4000, workflows);
+}
+
+function ProcessCondition(currentWorkflowName, xl, xu, ml, mu, al, au, sl, su, workflows)
+{
+  let currentWorkflow = workflows.find(w => w.Name === currentWorkflowName);
+  let routesToThisPoint = (xu - xl + 1) * (mu - ml + 1) * (au - al + 1) * (su - sl + 1);
+
+  for (let c = 0; c < currentWorkflow.Conditions.length; c++)
+  {
+    console.log(currentWorkflow.Conditions[c]);
+    let result = currentWorkflow.Conditions[c].Result;
+    
+    if (c === currentWorkflow.Conditions.length - 1)
+    {
+      switch(result)
+      {
+        case "R":
+          return;
+        case "A":
+          console.log(`Accepted amount here ${routesToThisPoint}`);  
+          return;
+        default:
+          ProcessCondition(result, xl, xu, ml, mu, al, au, sl, su, workflows); return;
+      }
+    }
+    else
+    {
+      let letter = currentWorkflow.Conditions[c].Letter;
+      let operator = currentWorkflow.Conditions[c].Operator;
+      let value = currentWorkflow.Conditions[c].Value;
+      switch (operator)
+      {
+        case ">":
+          switch (letter.toLowerCase())
+          {
+            case "x":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, value + 1, xu, ml, mu, al, au, sl, su, workflows); break;
+              }
+              break;
+            case "m":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, value + 1, mu, al, au, sl, su, workflows); break;
+              }
+              break;
+            case "a":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, ml, mu, value + 1, au, sl, su, workflows); break;
+              }
+              break;
+            case "s":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, ml, mu, al, au, value + 1, su, workflows); break;
+              }
+              break;
+            default: console.log("Letter not found"); break;
+          }
+          break;
+        case "<":
+          switch (letter.toLowerCase())
+          {
+            case "x":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, value - 1, ml, mu, al, au, sl, su, workflows); break;
+              }
+              break;
+            case "m":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, ml, value - 1, al, au, sl, su, workflows); break;
+              }
+              break;
+            case "a":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);   
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, ml, mu, al, value - 1, sl, su, workflows); break;
+              }
+              break;
+            case "s":
+              switch (result)
+              {
+                case "R":
+                  break;
+                case "A":
+                  console.log(`Accepted amount here ${routesToThisPoint}`);  
+                  break;
+                default:
+                  ProcessCondition(result, xl, xu, ml, mu, al, au, sl, value - 1, workflows); break;
+              }
+              break;
+            default: console.log("Letter not found"); break;
+          }
+          break;
+        default: console.log("No correct operator found"); break;
+      }
+    }
+  }
 }
 
 DayNineteenB();
