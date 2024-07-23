3ccd4f9014329afa9bc5dc1b51f77e713cdef0ba
felixreverett <felixreverett@gmail.com>
2024-07-23 20:28:24 +0100

    Day 3b progress

--- day-3/day-3b.js
+++ day-3/day-3b.js
@@ -5,20 +5,27 @@ var fs = require("fs"); // imports fs
 function DayThree()
 {
     // 1) first identify all symbols and mark around them in an array the same size as the input
+    
+    let lines = fs.readFileSync("day-3/sample-input-2023-3.txt", "utf-8")
+        .replace(/\r/gm, "")
+        .split("\n")
+        .map(i => i.split(""));
 
-    let lines = fs.readFileSync("day-3/input-2023-3.txt", "utf-8").split("\n").filter(line => line.trim() !== "");
-    
     // create valid gears array "mirror" structure
-    let validGearsArray = new Array(lines.length);
+    let validGearsArray = [];
     
-    for (let i = 0; i < lines.length; i++)
+    for (let r = 0; r < lines.length; r++)
     {
-        validGearsArray[i] = new Array(lines[i].length || 0).fill("."); // mark as . as filler
+        let newRow = [];
+        for (let c = 0; c < lines[r].length; c++)
+        {
+            newRow.push(".");
+        }
+        validGearsArray.push(newRow);
     }
 
-    let numberRegex = /[0-9]/;
-    let total = 0;
     // 3) Iterate through Lines to identify all "gears" *
+    let numberRegex = /[0-9]/;
     for (let row = 0; row < lines.length; row++)
     {
         for (let col = 0; col < lines[row].length; col++)
@@ -43,7 +50,7 @@ function DayThree()
                         }
 
                         // if there is a col to the right and that value is a number
-                        if (col < lines[row - 1].length - 1 && numberRegex.test(lines)[row - 1][col + 1])
+                        if (col < lines[row - 1].length - 1 && numberRegex.test(lines[row - 1][col + 1]))
                         {
                             adjacentNumbers++;
                         }
@@ -51,13 +58,13 @@ function DayThree()
                 }
 
                 // middle row (left)
-                if (col > 0 && numberRegex.test(lines)[row][col - 1])
+                if (col > 0 && numberRegex.test(lines[row][col - 1]))
                 {
                     adjacentNumbers++;
                 }
 
                 // middle row (right)
-                if (col < lines[row].length - 1 && numberRegex.test(lines)[row][col + 1])
+                if (col < lines[row].length - 1 && numberRegex.test(lines[row][col + 1]))
                 {
                     adjacentNumbers++;
                 }
@@ -81,7 +88,7 @@ function DayThree()
                         }
                         
                         // if there is a col to the right
-                        if (col < lines[row - 1].length - 1 && numberRegex.test(lines)[row + 1][col + 1])
+                        if (col < lines[row - 1].length - 1 && numberRegex.test(lines[row + 1][col + 1]))
                         {
                             adjacentNumbers++;
                         }
@@ -99,8 +106,56 @@ function DayThree()
 
     for (let i = 0; i < validGearsArray.length; i++)
     {
-        console.log(validGearsArray[i]);
+        let row = "";
+        for (let j = 0; j < validGearsArray[0].length; j++)
+        {
+            row += validGearsArray[i][j];
+        }
+        console.log(row);
+    }
+
+    // 4
+    let total = 0;
+    for (let r = 0; r < validGearsArray.length; r++)
+    {
+        for (let c = 0; c < validGearsArray[r].length; c++)
+        {
+            if (validGearsArray[r][c] === "*")
+            {
+                total += MultiplyAdjacentNumbers(r, c, lines);
+            }
+        }
     }
 }
 
 DayThree();
+
+function MultiplyAdjacentNumbers(r, c, lines)
+{
+    let numbers = [];
+    let numbersFound = 0;
+    let numberRegex = /[0-9]/;
+
+    while (numbersFound < 2)
+    {
+        // NNNNNNN
+        // NNN*NNN
+        // NNNNNNN
+        // look for numbers in row above
+        if (r > 0)
+        {
+            if (c - 2 > 0)
+            {
+                if (numberRegex.test(lines[r - 1][c - 3]))
+                {
+                    let currentNumber = lines[r - 1][c - 3];
+                    let a = 1;
+                    while (numberRegex.test(lines[r - 1][c - 3 + a]))
+                    {
+                        currentNumber = currentNumber * 10 + lines[r-1][c - 3 + a];
+                    }
+                }
+            }
+        }
+    }
+}
