0845e90ec9386835e5b020059a1486df29991e84
felixreverett <felixreverett@gmail.com>
2024-12-27 11:56:03 +0000

    🧩 day 7a solution

--- 07/solution.js
+++ 07/solution.js
@@ -1,13 +1,43 @@
 var fs = require("fs"); // imports fs
+console.time('a');
+
+function TestEquation(testValue, aggregateValue, calibrationValues) //int, int, []int
+{
+  if (calibrationValues.length === 1)
+  {
+    return (
+      testValue === aggregateValue * calibrationValues[0] ||
+      testValue === aggregateValue + calibrationValues[0]
+    );
+  }
+  else
+  {
+    return (
+      TestEquation(testValue, aggregateValue * calibrationValues[0], calibrationValues.slice(1)) ||
+      TestEquation(testValue, aggregateValue + calibrationValues[0], calibrationValues.slice(1))
+    );
+  }
+}
 
 function Solution()
 {
-  mappedArea = fs.readFileSync("07/sample-input.txt", "utf-8")
+  let calibrationEquations = fs.readFileSync("07/input.txt", "utf-8")
     .replace(/\r/gm, "")
+    .replace(/:\s/gm, " ")
     .split("\n")
-    .map(line => line.split(""));
+    .map(line => line.split(" ").map(number => parseInt(number)));
 
-  
+  let total = 0;
+
+  for (let e = 0; e < calibrationEquations.length; e++)
+  {
+    let testValue = calibrationEquations[e][0];
+    let calibrationValues = calibrationEquations[e].slice(1);
+    if (TestEquation(testValue, calibrationValues[0], calibrationValues.slice(1))) total += testValue;
+  }
+
+  console.timeEnd('a');
+  console.log(total);
 }
 
 Solution();
