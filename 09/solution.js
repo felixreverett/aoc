117144ad075e54907616a246f8a02548186e8a5c
Felix Everett <felixreverett@gmail.com>
2025-01-05 12:10:45 +0000

    🧩 day 09a solution (js)

--- 09/solution.js
+++ 09/solution.js
@@ -1,37 +1,79 @@
 var fs = require("fs"); // imports fs
+console.time('a');
 
-let compactedFile = "";
-
-function Solution()
+function GetCheckSum(diskMap)
 {
-  let diskMap = fs.readFileSync("09/sample-input.txt", "utf-8")
-    .replace(/\r/gm, "")
-    .split("")
-    .map(int => parseInt(int));
-  
-  console.log(diskMap.length);
+  let checkSum = 0;
+  let iterator = 0;
 
-  let rightPointer = diskMap.length;
-
-  for (let leftPointer = 0; leftPointer < rightPointer; leftPointer += 2)
+  for (let a = 0; a < diskMap.length; a++)
   {
-    // append fileID to the compacted file "diskMap[leftPointer] times"
-    let fileID = leftPointer / 2;
-    
-    for (let i = 0; i < diskMap[leftPointer]; i++)
+    for (let v = 0; v < diskMap[a].length; v++)
     {
-      compactedFile += fileID;
-    }
-
-    // shift fileID values at the end of the diskMap to the compated file "diskMap[leftPointer+1] times"
-    let freeSpace = fileID + 1;
-    for (let space = 0; space < diskMap[leftPointer + 1]; space++)
-    {
-      // realising that this approach may not work.
+      if (diskMap[a][v] === ".") return checkSum;
+      checkSum += iterator * diskMap[a][v];
+      iterator++;
     }
   }
 
-  console.log(compactedFile);
+  return checkSum;
+}
+
+function Solution()
+{
+  let diskMap = fs.readFileSync("09/input.txt", "utf-8")
+    .replace(/\r/gm, "")
+    .split("")
+    .map((char, index) => 
+    {
+      let int = parseInt(char);
+      let array = [];
+      if (index % 2 === 0)
+      {
+        let fileID = index / 2;
+        for (let i = 0; i < int; i++)
+        {
+          array.push(fileID);
+        }
+      }
+      else
+      {
+        for (let i = 0; i < int; i++)
+        {
+          array.push(".");
+        }
+      }
+      return array;
+    });
+
+  let leftPointer = 0;
+  let rightPointer = diskMap.length - 1;
+  let rightSubPointer = diskMap[rightPointer].length - 1;
+
+  while (leftPointer < rightPointer)
+  {
+    let leftSubPointer = 0;
+
+    while (leftSubPointer < diskMap[leftPointer + 1].length)
+    {
+      if (rightSubPointer < 0)
+      {
+        rightPointer -= 2;
+        if (rightPointer <= leftPointer) break;
+        rightSubPointer = diskMap[rightPointer].length - 1;
+      }
+            
+      [diskMap[leftPointer + 1][leftSubPointer], diskMap[rightPointer][rightSubPointer]] = [diskMap[rightPointer][rightSubPointer], diskMap[leftPointer + 1][leftSubPointer]];
+      
+      leftSubPointer++;
+      rightSubPointer--;
+    }
+
+    leftPointer += 2;
+  }
+
+  console.timeEnd('a');
+  console.log(GetCheckSum(diskMap)); //solution
 }
 
 Solution();
