3e5871ce305ef9981d68b8dcd18152e4edf56411
Felix Everett <felixreverett@gmail.com>
2025-03-12 19:08:49 +0000

    ⚙️ day 13a theoretical workings (learning linear algebra)

--- 13/solution.js
+++ 13/solution.js
@@ -2,7 +2,24 @@ var fs = require("fs"); // imports fs
 console.time('a');
 
 // Plan (13a):
-// 
+/*
+Button A: X+94, Y+34    a, c
+Button B: X+22, Y+67    b, d
+Prize: X=8400, Y=5400   e, f
+
+( 94 22 ) (A) = ( 8400 ) 
+( 34 67 ) (B) = ( 5400 )
+
+( a b ) (A) = ( e )
+( c d ) (B) = ( f )
+
+det := ad-bc
+
+( A ) = 1 / det * ( d -b ) ( e ) = ( ( d / det) * e + (-b / det) * f )
+( B ) =           ( -c a ) ( f ) = ( (-c / det) * e + ( a / det) * f )
+
+A = 
+*/
 
 function Solution()
 {
