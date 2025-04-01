c51da14935b218d5e86f493fcacb965834e35747
Felix Everett <felixreverett@gmail.com>
2025-04-01 19:20:03 +0100

    ⚙️ day 15a progress

--- 15/solution.js
+++ 15/solution.js
@@ -2,13 +2,65 @@ var fs = require("fs"); // imports fs
 console.time('a');
 
 /* Plan 15a
-> 
+> Break input into warehouse map and instruction set
 */
 
-function Solution(mapWidth, mapHeight, seconds)
+function FindRobot(x, y)
 {
-  let input = fs.readFileSync("15/input.txt", "utf-8")
-    .replace(/\r/gm, "");
+  //
+}
+
+function IsInBounds(x, y)
+{
+  //
+}
+
+function ProcessInstruction(map, instruction, x, y)
+{
+  // Process instruction
+  // If x and y are not -1 and @ is at location of x y, use this value
+  // Otherwise find @
+  if (!(IsInBounds(x, y) && map[x][y] === "@"))
+  {
+    x, y = FindRobot(map);
+  }
+  
+  let offset = [];
+  switch (instruction) {
+    case "^":
+      offset = [-1, 0];
+      break;
+    case ">":
+      offset = [0, 1];
+      break;
+    case "v":
+      offset = [1, 0];
+      break;
+    case "<":
+      offset = [0, -1];
+      break;
+    default:
+      console.error("instruction type not found.");
+  }
+}
+
+function Solution()
+{
+  let mapAndInstructions = fs.readFileSync("15/sample-input.txt", "utf-8")
+    .replace(/\r/gm, "")
+    .split("\n\n");
+
+  let map = mapAndInstructions[0].split("\n").map(i => i.split(""));
+  let instructions = mapAndInstructions[1].replace(/\n/gm, "").split("");
+
+  let x = -1;
+  let y = -1;
+
+  instructions.forEach(instruction => {
+    x, y = ProcessInstruction(map, instruction, x, y);
+  });
+
+  console.log(map);
 
   let solution = 0;
   
