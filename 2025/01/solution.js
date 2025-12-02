bca064822f61b96a6e14182c257dfb3900b5cac6
Felix Everett <felixreverett@gmail.com>
2025-12-02 15:18:01 +0000

    🧹 day 1b improvements

--- 2025/01/solution.js
+++ 2025/01/solution.js
@@ -15,7 +15,7 @@ function PartOne() {
     {
         dialLocation = (((dialLocation+input[i][0]*input[i][1]) % 100) + 100) % 100;
         dialLocation === 0 ? timesAtZero++ : timesAtZero;
-        console.log(`dial location is now ${dialLocation}`);
+        //console.log(`dial location is now ${dialLocation}`);
     }
 
     console.log(timesAtZero);
@@ -34,26 +34,9 @@ function PartTwo() {
     {
         let oldLocation = dialLocation;
         dialLocation += direction * magnitude;
-        let cycles = 0;
-
         if (dialLocation == 0) { TotalTimesPassedZero++ }
-
-        if (dialLocation < 0) {
-            if (oldLocation == 0) {
-                cycles = Math.abs(dialLocation)/100|0
-            } else {
-                cycles = (Math.abs(dialLocation)/100|0)+1;
-            }
-        } else {
-            cycles = Math.abs(dialLocation)/100|0;
-        }
-        console.log(`Value moved ${direction*magnitude} from ${oldLocation} to ${dialLocation}`);
-        console.log(`Crossed zero ${cycles} times.`);
-        TotalTimesPassedZero += cycles;
-        
+        TotalTimesPassedZero += (Math.abs(dialLocation)/100|0) + (dialLocation < 0 && oldLocation !== 0);
         dialLocation = (((dialLocation) % 100) + 100) % 100;
-        
-        console.log(`Value was then clamped to ${dialLocation}\n=====`);
     }
 
     console.log(TotalTimesPassedZero);
