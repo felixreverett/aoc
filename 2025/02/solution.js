68a9893589336aaf0812c43ce8901d003c5f4054
Felix Everett <felixreverett@gmail.com>
2025-12-04 14:45:26 +0000

    🧩 day 2 parts 1 and 2

---
+++ 2025/02/solution.js
@@ -0,0 +1,50 @@
+const fs = require("fs"); // imports file management
+const path = require("path");
+const filePath = path.join(__dirname, "input.txt");
+
+function PartOne() {
+    let input = fs.readFileSync(filePath, "utf-8")
+        .replace(/\r|\n/gm, "")
+        .split(",")
+        .map(i => i.split("-").map(i => +i));
+
+    const re = /^(.+)\1$/;
+    let total = 0;
+
+    for (let [i, j] of input) {
+        while (i <= j) {
+            i2 = ""+i;
+            if (i2.length && 1 != 0 && re.test(i2)) {
+                total += i;
+            }
+            i++;
+        }
+    }
+
+    console.log(total);
+}
+
+function PartTwo() {
+    let input = fs.readFileSync(filePath, "utf-8")
+        .replace(/\r|\n/gm, "")
+        .split(",")
+        .map(i => i.split("-").map(i => +i));
+
+    const re = /^(.+)\1+$/;
+    let total = 0;
+
+    for (let [i, j] of input) {
+        while (i <= j) {
+            i2 = ""+i;
+            if (i2.length && 1 != 0 && re.test(i2)) {
+                total += i;
+            }
+            i++;
+        }
+    }
+
+    console.log(total);
+}
+
+PartOne();
+PartTwo();
