c5b29db1eac7e975c3952a798f317b587a1f04ab
Felix Everett <felixreverett@gmail.com>
2025-12-05 14:35:57 +0000

    🧩 day 3 part 2 solution

--- 2025/03/solution.js
+++ 2025/03/solution.js
@@ -9,7 +9,6 @@ function PartOne() {
         .split("\n")
         .map(i => i.split("").map(i => +i));
 
-    console.log(input);
     let total = 0;
 
     for (let r = 0; r < input.length; r++) {
@@ -45,6 +44,43 @@ function PartOne() {
     console.log(total);
 }
 
+function PartTwo() {
+    console.time('a');
+    let input = fs.readFileSync(filePath, "utf-8")
+        .replace(/\r/gm, "")
+        .split("\n")
+        .map(i => i.split("").map(i => +i));
+
+    let total = 0;
+    let valueLength = 12;
+
+    for (let r = 0; r < input.length; r++) {
+        let largestUnitsInRow = [];
+        let largestPos = -1;
+        for (let unit = 0; unit < valueLength; unit++) {
+            let largestVal = -1;
+            for (let lPointer = largestPos+1; lPointer < input[r].length-valueLength+unit+1; lPointer++) {
+                let currentVal = input[r][lPointer];
+                if (currentVal > largestVal) {
+                    largestVal = currentVal;
+                    largestPos = lPointer;
+                    if (currentVal == 9) {
+                        break;
+                    }
+                }
+            }
+            largestUnitsInRow.push(largestVal);
+        }
+        let subtotal = 0;
+        for (let u = 0; u < largestUnitsInRow.length; u++) {
+            subtotal+=largestUnitsInRow[u]*10**(valueLength-u-1);
+        }
+        total += subtotal;
+    }
+    console.timeEnd('a');
+    console.log(total);
+}
+
 
 PartOne();
-//PartTwo();
+PartTwo();
