925ca5d11f4e7ac09da745902a79c7d288abf620
Felix Everett <felixreverett@gmail.com>
2025-12-21 11:45:54 +0000

    🧩 Day 11 Part 2 Solution

--- 2025/11/solution.py
+++ 2025/11/solution.py
@@ -33,9 +33,53 @@ def traverse_route(key, values, memo, devicesmaps):
         total += memo[v]
     memo[key] = total
     return total
+
+def Part_Two():
+    start_time = time.perf_counter()
+    with open(file_path, "r") as file_handle:
+        devicesmaps = {
+            parts[0]: [item.strip() for item in parts[1].split(" ")]
+            for row in file_handle.read().strip().split("\n")
+            for parts in [row.split(": ")]
+        }
+    
+    memo = {}
+
+    has_crossed_fft = False
+    has_crossed_dac = False
+
+    total = traverse_route_two("svr", memo, devicesmaps, has_crossed_fft, has_crossed_dac)
+
+    print(f"Finished in {time.perf_counter() - start_time:.4f} seconds") 
+    print(total)
             
+def traverse_route_two(key, memo, devicesmaps, has_crossed_fft, has_crossed_dac):
+    
+    if key == "fft":
+        has_crossed_fft = True
+    elif key == "dac":
+        has_crossed_dac = True
+    
+    if key == "out":
+        return 1 if has_crossed_fft and has_crossed_dac else 0
+    
+    state = (key, has_crossed_fft, has_crossed_dac)
+    if state in memo:
+        return memo[state]
+
+    total = 0
+    vals = devicesmaps.get(key, [])
+
+    for v in vals:
+        total += traverse_route_two(v, memo, devicesmaps, has_crossed_fft, has_crossed_dac)
+
+    memo[state] = total
+
+    return total
+
 def main():
     Part_One()
+    Part_Two()
 
 if __name__ == "__main__":
     main()
