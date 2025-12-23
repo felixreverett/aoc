f25b2348a0f03f0cfc39d9432436abcce1fd0a0b
Felix Everett <felixreverett@gmail.com>
2025-12-23 12:11:04 +0000

    🧩 Day 9 Part 2 Solution

--- 2025/09/solution.py
+++ 2025/09/solution.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 import time
-import math
-from collections import defaultdict
+from collections import deque
 
-file_path = Path(__file__).parent / "sample-input.txt"
+file_path = Path(__file__).parent / "input.txt"
 
 def Part_One():
-    start_time = time.perf_counter()
     with open(file_path, "r") as file_handle:
         file = file_handle.read().replace(r"\rgm", "").split("\n")
     tiles = [[int(val) for val in row.split(",")] for row in file]
@@ -19,162 +17,110 @@ def Part_One():
             if new_area > largest_area:
                 largest_area = new_area
 
-    print(f"Finished in {time.perf_counter() - start_time:.4f} seconds") 
-    print(largest_area)
-
-def Part_Two_v2():
-    start_time = time.perf_counter()
-    with open(file_path, "r") as file_handle:
-        file = file_handle.read().replace(r"\rgm", "").split("\n")
-    tiles = [[int(val) for val in row.split(",")] for row in file]
-
-    outline_pixels = set()
-
-    for i in range(len(tiles)):
-        p1 = tiles[i]
-        
-        
+    return largest_area
 
 def Part_Two():
-    start_time = time.perf_counter()
     with open(file_path, "r") as file_handle:
         file = file_handle.read().replace(r"\rgm", "").split("\n")
     tiles = [[int(val) for val in row.split(",")] for row in file]
+    num_tiles = len(tiles)
 
-    row_map = defaultdict(list)
+    # Coordinate compression with padding for later flood fill :)
+    unique_xs = set(t[0] for t in tiles)
+    unique_ys = set(t[1] for t in tiles)
 
-    edges = []
-    num_nodes = len(tiles)
-    for i in range(num_nodes):
-        for j in range(i + 1, num_nodes):
-            dist = math.dist(tiles[i], tiles[j])
-            edges.append((dist, tiles[i], tiles[j]))
+    unique_xs.add(min(unique_xs) - 1)
+    unique_xs.add(max(unique_xs) + 1)
+    unique_ys.add(min(unique_ys) - 1)
+    unique_ys.add(max(unique_ys) + 1)
 
-    edges.sort(key=lambda x: x[0])
+    sorted_xs = sorted(list(unique_xs))
+    sorted_ys = sorted(list(unique_ys))
 
-    limit = len(tiles) * 2
-    active_edges = edges[:limit]
+    x_map = {val: i * 2 for i, val in enumerate(sorted_xs)}
+    y_map = {val: i * 2 for i, val in enumerate(sorted_ys)}
 
-    for _, p1, p2 in active_edges:
-        x1, y1 = p1
-        x2, y2 = p2
-        dx = abs(x2 - x1)
-        dy = abs(y2 - y1)
-        steps = math.gcd(dx, dy)
+    width = len(sorted_xs) * 2
+    height = len(sorted_ys) * 2
 
-        step_x = (x2 - x1) // steps
-        step_y = (y2 - y1) // steps
+    compressed_grid = [[0 for _ in range(width)] for _ in range(height)]
 
-        for i in range(steps + 1):
-            curr_x = x1 + (i * step_x)
-            curr_y = y1 + (i * step_y)
+    compressed_tiles = [(x_map[t[0]], y_map[t[1]]) for t in tiles]
 
-            if y1 == y2:
-                row_map[y1].append([min(x1, x2), max(x1, x2)])
-                break
+    for i in range(num_tiles):
+        p1 = compressed_tiles[i]
+        p2 = compressed_tiles[(i + 1) % num_tiles]
 
-            row_map[curr_y].append([curr_x, curr_x])
+        c1, r1 = p1
+        c2, r2 = p2
 
-    for y in row_map:
-        intervals = sorted(row_map[y])
-        merged = []
-        if intervals:
-            curr_start, curr_end = intervals[0]
-            for next_start, next_end in intervals[1:]:
-                if next_start <= curr_end + 1:
-                    curr_end = max(curr_end, next_end)
-                else:
-                    merged.append((curr_start, curr_end))
-                    curr_start, curr_end = next_start, next_end
-            merged.append((curr_start, curr_end))
-        row_map[y] = merged
+        if c1 == c2: # vertical line
+            for row in range(min(r1, r2), max(r1, r2) + 1):
+                compressed_grid[row][c1] = 1 # wall
+        else:
+            for col in range(min(c1, c2), max(c1, c2) + 1):
+                compressed_grid[r1][col] = 1 # wall
 
-    # section 2
-    candidates = []
-    for a in range(len(tiles)-1):
-        for b in range(a+1, len(tiles)):
-            p1 = tiles[a]
-            p2 = tiles[b]
-            w = abs(p2[0] - p1[0]) + 1
-            h = abs(p2[1] - p1[1]) + 1
-            candidates.append((w * h, p1, p2))
-    candidates.sort(key=lambda x: x[0], reverse=True)
+    # Flood filling the outside
+    queue = deque([(0, 0)])
 
-    largest_valid_area = 0
+    if compressed_grid[0][0] == 0:
+        compressed_grid[0][0] = -1
 
-    for area, p1, p2 in candidates:
-        min_x, max_x = sorted((p1[1], p2[0]))
-        min_y, max_y = sorted((p1[1], p2[1]))
-        is_valid = True
-
-        # row scan
-        for y in range(min_y, max_y + 1):
-            if y not in row_map:
-                is_valid = False
-                break
-
-            # checking if [min_x, max_x] fits inside any merged intervals
-            row_covered = False
-            for interval in row_map[y]:
-                i_start, i_end = interval
-                if i_start <= min_x and i_end >= max_x:
-                    row_covered = True
-                    break
-            
-            if not row_covered:
-                is_valid = False
-                break
-
-        if is_valid:
-            largest_valid_area = area
-            break
-    
-    print(f"Finished in {time.perf_counter() - start_time:.4f} seconds") 
-    print(f"Largest Valid Area: {largest_valid_area}")
-
-    """
-    areas = []
-
-    for a in range(len(tiles)-1):
-        for b in range(a+1, len(tiles)):
-            p1 = tiles[a]
-            p2 = tiles[b]
-            width = abs(p2[0] - p1[0]) + 1
-            height = abs(p2[1] - p1[1]) + 1
-            area = width * height
-
-            if area > total_tile_count:
-                continue
-
-            areas.append((area, p1, p2))
-
-    areas.sort(reverse=True)
-
-    # 
-    largest_valid_area = 0
-
-    for area, p1, p2 in areas:
-        min_x, max_x = sorted((p1[0], p2[0]))
-        min_y, max_y = sorted((p1[1], p2[1]))
-
-        is_valid = True
-
-        for y in range(min_y, max_y + 1):
-            for x in range(min_x, max_x + 1):
-                if (x, y) not in tile_set:
-                    is_valid = False
-                    break
-            if not is_valid:
-                break
+    while queue:
+        cx, cy = queue.popleft()
         
-        if is_valid:
-            largest_valid_area = area
-            break"""
+        for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+            nx, ny = cx + dx, cy + dy
+            
+            if 0 <= nx < width and 0 <= ny < height:
+                if compressed_grid[ny][nx] == 0:
+                    compressed_grid[ny][nx] = -1
+                    queue.append((nx, ny))
 
+    # Summed area table
+    sums = [[0 for _ in range(width + 1)] for _ in range(height + 1)]
 
-def main():
-    Part_One()
-    Part_Two()
+    for y in range(1, height + 1):
+        for x in range(1, width + 1):
+            is_invalid = 1 if compressed_grid[y-1][x-1] == -1 else 0
+            sums[y][x] = is_invalid + sums[y-1][x] + sums[y][x-1] - sums[y-1][x-1]
+        
+    def get_invalid_count(x1, y1, x2, y2):
+        x_min, x_max = min(x1, x2), max(x1, x2)
+        y_min, y_max = min(y1, y2), max(y1, y2)
+        
+        return (sums[y_max + 1][x_max + 1] 
+              - sums[y_min][x_max + 1] 
+              - sums[y_max + 1][x_min] 
+              + sums[y_min][x_min])
+        
+    largest_area = 0
+
+    # Iterate through all pairs of tiles
+    for i in range(num_tiles):
+        for j in range(i + 1, num_tiles):
+            cx1, cy1 = compressed_tiles[i]
+            cx2, cy2 = compressed_tiles[j]
+            if get_invalid_count(cx1, cy1, cx2, cy2) == 0:
+                
+                orig_x1, orig_y1 = sorted_xs[cx1 // 2], sorted_ys[cy1 // 2]
+                orig_x2, orig_y2 = sorted_xs[cx2 // 2], sorted_ys[cy2 // 2]
+                
+                area = (abs(orig_x2 - orig_x1) + 1) * (abs(orig_y2 - orig_y1) + 1)
+                
+                if area > largest_area:
+                    largest_area = area
+    
+    return largest_area
 
 if __name__ == "__main__":
-    main()
+    start_time = time.perf_counter()
+    solution = Part_One()
+    print(f"Finished in {time.perf_counter() - start_time:.4f} seconds") 
+    print(f"Part 1 Solution: {solution}")
+
+    start_time = time.perf_counter()
+    solution = Part_Two()
+    print(f"Finished in {time.perf_counter() - start_time:.4f} seconds") 
+    print(f"Part 2 Solution: {solution}")
