803dc9dde3b32aa215d7ae7ca7731240f26ed8fd
Felix Everett <felixreverett@gmail.com>
2026-01-02 13:47:11 +0000

    🧩 Day 4 Part 1 Solution

--- 2022/04/solution.scala
+++ 2022/04/solution.scala
@@ -4,13 +4,31 @@ import scala.io.Source
 import java.io.File
 import scala.compiletime.ops.string
 
+case class Range(start: Int, end: Int) {
+    def contains(other: Range): Boolean = 
+        this.start <= other.start && this.end >= other.end
+
+    def overlaps(other: Range): Boolean =
+        this.start <= other.end && other.start <= this.end
+}
+
 def partOne(): (Int, Double) = {
-    val filename = "2022/04/sample-input.txt"
+    val filename = "2022/04/input.txt"
     val input = Source.fromFile(new File(filename)).mkString
 
     val startTime = System.nanoTime()
     
-    val solution = 1
+    val solution = input
+        .split("\r?\n")
+        .view
+        .filter(_.trim.nonEmpty)
+        .map { 
+            case s"${s1}-${e1},${s2}-${e2}" =>
+                (Range(s1.toInt, e1.toInt), Range(s2.toInt, e2.toInt))
+        }
+        .count { (rangeA, rangeB) =>
+            rangeA.contains(rangeB) || rangeB.contains(rangeA)
+        }
 
     val duration = (System.nanoTime() - startTime) / 1e6
 
