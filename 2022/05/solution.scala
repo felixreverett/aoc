4b8fce6633ed43a11757d5e4546c214dd33b2049
Felix Everett <felixreverett@gmail.com>
2026-01-06 10:48:31 +0000

    🧩 Day 5 Solutions

--- 2022/05/solution.scala
+++ 2022/05/solution.scala
@@ -4,18 +4,18 @@ import scala.io.Source
 import java.io.File
 import scala.compiletime.ops.string
 
-def partOne(): (Int, Double) = {
-    val filename = "2022/05/sample-input.txt"
+def partOne(): (String, Double) = {
+    val filename = "2022/05/input.txt"
     val input = Source.fromFile(new File(filename)).mkString
 
     val startTime = System.nanoTime()
 
-    val solution = input
+    val (initialStacks, instructions) = input
         .split("""(\r?\n){2}""")
         .toList match {
             case List(s, i) =>
                 val stacks = s
-                    .split("""(\r?\n)""")
+                    .split("(\r?\n)")
                     .map(line => line.grouped(4).map(_.charAt(1)).toArray)
                     .transpose
                     .map(_.filter(_.isLetter).reverse)
@@ -28,24 +28,62 @@ def partOne(): (Int, Double) = {
                 (stacks, instructions)
             case _ => throw new Exception("Could not split into stacks and instructions")
         }
-        .map { (stacks, instructions) =>
-            instructions.foreach { i =>
-                (0 until i[0]).foreach {
-                    movedVal = stacks[i][1].pop
-                    stacks[i][2].push(movedVal)
-                }
-            }
+
+    val solution = instructions.foldLeft(initialStacks) { (currentStacks, instr) =>
+        val (amount, from, to) = instr
+        val srcIndex = from - 1
+        val destIndex = to - 1
+        val toMove = currentStacks(srcIndex).takeRight(amount)
+        val newSource = currentStacks(srcIndex).dropRight(amount)
+        val newDest = currentStacks(destIndex) ++ toMove.reverse
+        currentStacks.updated(srcIndex, newSource).updated(destIndex, newDest)
+    }
+    .map(_.last).mkString
+
+    val duration = (System.nanoTime() - startTime) / 1e6
+
+    (solution, duration)
+}
+
+def partTwo(): (String, Double) = {
+    val filename = "2022/05/input.txt"
+    val input = Source.fromFile(new File(filename)).mkString
+
+    val startTime = System.nanoTime()
+
+    val (initialStacks, instructions) = input
+        .split("""(\r?\n){2}""")
+        .toList match {
+            case List(s, i) =>
+                val stacks = s
+                    .split("(\r?\n)")
+                    .map(line => line.grouped(4).map(_.charAt(1)).toArray)
+                    .transpose
+                    .map(_.filter(_.isLetter).reverse)
+                val instructions = i
+                    .split("\r?\n")
+                    .map { 
+                        case s"move $amount from $src to $dest" =>
+                            (amount.toInt, src.toInt, dest.toInt)
+                    }
+                (stacks, instructions)
+            case _ => throw new Exception("Could not split into stacks and instructions")
         }
 
-    val (stacks, _) = solution
+    val solution = instructions.foldLeft(initialStacks) { (currentStacks, instr) => 
+        val (amount, from, to) = instr
+        val srcIndex = from - 1
+        val destIndex = to - 1
+        val toMove = currentStacks(srcIndex).takeRight(amount)
+        val newSource = currentStacks(srcIndex).dropRight(amount)
+        val newDest = currentStacks(destIndex) ++ toMove
+        currentStacks.updated(srcIndex, newSource).updated(destIndex, newDest)    
+    }
+    .map(_.last).mkString
 
-    println("Current State of Stacks:")
-    stacks.indices.foreach { i =>
-        val content = stacks(i).mkString(" ")
-        println(s"${i + 1} | $content")
-        }
+    val duration = (System.nanoTime() - startTime) / 1e6
 
-    (1, 1.0f)
+    (solution, duration)
 }
 
 @main def run(): Unit = {
@@ -53,7 +91,7 @@ def partOne(): (Int, Double) = {
     println(s"Part 1 Solution: ${p1}")
     println(s"Time Part 1: ${d1}ms")
 
-    //val (p2, d2) = partTwo()
-    //println(s"Part 2 Solution: ${p2}")
-    //println(s"Time Part 2: ${d2}ms")
+    val (p2, d2) = partTwo()
+    println(s"Part 2 Solution: ${p2}")
+    println(s"Time Part 2: ${d2}ms")
 }
