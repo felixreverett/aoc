8e1a099f6b2401e97a4c58b7b0d0c741163f9e60
Felix Everett <felixreverett@gmail.com>
2026-01-07 10:15:52 +0000

    🧩 Day 6 Solutions

---
+++ 2022/06/solution.scala
@@ -0,0 +1,55 @@
+package aoc2022.day6
+
+import scala.io.Source
+import java.io.File
+import scala.compiletime.ops.string
+
+def partOne(): (Int, Double) = {
+    val filename = "2022/06/input.txt"
+    val input = Source.fromFile(new File(filename)).mkString
+
+    val startTime = System.nanoTime()
+
+    val solution = input
+        .sliding(4)
+        .zipWithIndex
+        .find { case (str, i) =>
+            str.distinct.length == 4 
+        }
+        .map { case (str, i) => i }
+        .getOrElse(0) + 4
+
+    val duration = (System.nanoTime() - startTime) / 1e6
+
+    (solution, duration)
+}
+
+def partTwo(): (Int, Double) = {
+    val filename = "2022/06/input.txt"
+    val input = Source.fromFile(new File(filename)).mkString
+
+    val startTime = System.nanoTime()
+
+    val solution = input
+        .sliding(14)
+        .zipWithIndex
+        .find { case (str, i) =>
+            str.distinct.length == 14 
+        }
+        .map { case (str, i) => i }
+        .getOrElse(0) + 14
+
+    val duration = (System.nanoTime() - startTime) / 1e6
+
+    (solution, duration)
+}
+
+@main def run(): Unit = {
+    val (p1, d1) = partOne()
+    println(s"Part 1 Solution: ${p1}")
+    println(s"Time Part 1: ${d1}ms")
+
+    val (p2, d2) = partTwo()
+    println(s"Part 2 Solution: ${p2}")
+    println(s"Time Part 2: ${d2}ms")
+}
