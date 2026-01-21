870767c265feaec02471548e044dae13b019e39b
Felix Everett <felixreverett@gmail.com>
2026-01-21 14:06:53 +0000

    🧩 Day 9 Part 1 Solution (TS)

--- 2022/09/solution.ts
+++ 2022/09/solution.ts
@@ -10,30 +10,47 @@ function PartOne(path: string): { solution: number; duration: string} {
         .split("\n")
         .map(i => {
             const j = i.split(" ");
-            return [j[0], parseInt(j[1])];
+            return [j[0], parseInt(j[1])] as [string, number];
         });
 
-    
     let headPosition = [0, 0];
-    let tailPosition = headPosition;
+    let tailPosition = [0, 0];
 
     let visitedPositions = new Set<string>();
+    visitedPositions.add(`0,0`);
 
     for (let i = 0; i < input.length; i++) {
-        const {direction, magnitude} = input[i];
-        const vector = (direction: string) => ({
-            'R': [0, 1],
-            'U': [-1, 0],
-            'L': [0, -1],
-        }[direction] || [1,0]);
+
+        const [direction, magnitude] = input[i];
+
+        let vector: [number, number];
+
+        switch (direction) {
+            case 'R':
+                vector = [0, 1];
+                break;
+            case 'U':
+                vector = [-1, 0];
+                break;
+            case 'L':
+                vector = [0, -1];
+                break;
+            case 'D':
+            default:
+                vector = [1, 0];
+                break;
+        }
+
         for (let j = 1; j <= magnitude; j++) {
             headPosition[0] += vector[0];
             headPosition[1] += vector[1];
-            const rowDifference = headPosition[0] - tailPosition[0]
-            const colDifference = headPosition[1] - tailPosition[1]
+
+            const rowDifference = Math.abs(headPosition[0] - tailPosition[0]);
+            const colDifference = Math.abs(headPosition[1] - tailPosition[1]);
+
             if (rowDifference > 1) {
                 tailPosition[0] += vector[0];
-                tailPostion[1] = headPosition[1];
+                tailPosition[1] = headPosition[1];
             } else if (colDifference > 1) {
                 tailPosition[1] += vector[1];
                 tailPosition[0] = headPosition[0];
@@ -43,17 +60,13 @@ function PartOne(path: string): { solution: number; duration: string} {
         }
     }
 
-    console.log(input);
-    // do something here
-
-    let solution = 0
     const duration = (performance.now() - start).toFixed(4) + 'ms'
 
-    return { solution: solution, duration: duration}
+    return { solution: visitedPositions.size, duration: duration}
 }
 
 function main(): void {
-    const filePath = path.join(__dirname, "sample-input.txt");
+    const filePath = path.join(__dirname, "input.txt");
 
     const {solution, duration} = PartOne(filePath);
     console.log(`Part 1 Solution: ${solution}\nTime Part 1: ${duration}`);
